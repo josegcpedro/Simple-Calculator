d82669e864c81624c4cffcf8b899f9c2757ec408
Jose <pedrojcunha7@hotmail.com>
2025-01-08 14:56:10 +0100

    feat: implement division logic with input validation

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,8 +10,8 @@ class Program
         Console.WriteLine("2. Soustraction");
         Console.WriteLine("3. Multiplication");
         Console.WriteLine("4. Division");
-        Console.WriteLine("5. Modulo")
-        Console.WriteLine("5. Puissances")
+        Console.WriteLine("5. Modulo");
+        Console.WriteLine("6. Puissances");
 
         string choix = Console.ReadLine();
 
@@ -30,7 +30,13 @@ class Program
                 Multiplication();
                 break;
             case "4":
-                //Division();
+                Division();
+                break;
+            case "5":
+                //Modulo();
+                break;
+            case "6":
+                //Puissances();
                 break;
             default:
                 Console.WriteLine("erreur");
@@ -84,6 +90,24 @@ class Program
         if (int.TryParse(reponse1, out int chiffre1) && int.TryParse(reponse2, out int chiffre2))
         {
             int resultat = chiffre1 * chiffre2;
+            Console.WriteLine("Le resultat de votre opération est de: " + resultat);
+        }
+        else
+        {
+            Console.WriteLine("Veuillez mettre des chiffres");
+        }
+    }
+
+    static void Division()
+    {
+        Console.WriteLine("Donnez votre premier chiffre");
+        string reponse1 = Console.ReadLine();
+        Console.WriteLine("Donnez votre deuxième chiffre");
+        string reponse2 = Console.ReadLine();
+
+        if (float.TryParse(reponse1, out float chiffre1) && float.TryParse(reponse2, out float chiffre2))
+        {
+            float resultat = chiffre1 / chiffre2;
             Console.WriteLine("Le resultat de votre opération est de: " + resultat);
         }
         else
