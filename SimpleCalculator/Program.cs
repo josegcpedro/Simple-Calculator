90028d3e1cf1868435b98ac033b8ec9b50476faf
Jose <pedrojcunha7@hotmail.com>
2025-01-08 14:24:13 +0100

    feat: add interactive menu with operations choises

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bienvenue à cette simple calculatrice!");
+        Console.WriteLine("Veuillez choisir les options suivantes :");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Multiplication");
+        Console.WriteLine("3. Soustraction");
+        Console.WriteLine("4. Division");
+
+        string choix = Console.ReadLine();
+
+        Console.WriteLine(choix);
+
+
+        switch (choix)
+        {
+            case "Addition":
+                Addition();
+                break;
+            case "Multiplication":
+                Multiplication();
+                break;
+            case "Soustraction":
+                Soustraction();
+                break;
+            case "Division":
+                Division();
+                break;
+            default:
+                Console.ReadLine("erreur");
+                break;
+        }
+    }
+
+
+}
